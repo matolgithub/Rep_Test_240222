32f705d7a3bd60fd93b700e608a28835d7302937
Олег Матшин <89175955650@mail.ru>
2022-02-24 22:18:55 +0300

    Add new condition if/elif

--- test_range_160122_2107.py
+++ test_range_160122_2107.py
@@ -1,4 +1,10 @@
 x = input('enter something: ')
+if x.isdigit():
+    print('You enter digits')
+elif x.isalpha():
+    print('You enter simbols')
+else:
+    print('You enter something!')
 print(x, type(x))
 print(x[0:3])
 print(x[::-1])
